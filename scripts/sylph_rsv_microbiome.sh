3a3917a0fae3da010ea583fc990fa01c332e750a
lambda-quad <dbhaslam@gmail.com>
2025-03-29 16:33:14 -0400

    modified:   scripts/sylph_rsv_microbiome.sh

--- scripts/sylph_rsv_microbiome.sh
+++ scripts/sylph_rsv_microbiome.sh
@@ -2,14 +2,14 @@
 # databases are downloaded into ~/Databases/BacterialDatabases/ ../FungalDatabases ../ViralDatabases
 # the databases are preconfigured sketches for use in sylph
 
-cd ~/Data/MSSData
+cd ~/Analysis/TrimmedMSSFiles/
 
 files=$(cat ~/Documents/Code/RSV-microbiome-2025/sample_list.txt)
 for f in $files; do
 
 cd ~/Analysis/TrimmedMSSFiles/
 
-/usr/local/sylph sketch -1 trimmed_read1_${f}.fastq.gz -2 trimmed_read2_${f}.fastq.gz -d Sketches/${f}.sylsp
+sylph sketch -1 trimmed_read1_${f}.fastq.gz -2 trimmed_read2_${f}.fastq.gz -d Sketches/${f}.sylsp
 
 done
 
@@ -21,19 +21,19 @@ cd ~/Analysis/TrimmedMSSFiles/
 
 # Bacterial query and profiling
 
-/usr/local/sylph query  ~/Databases/BacterialDatabases/v0.3-c200-gtdb-r214.syldb Sketches/${f}.sylsp/trimmed_read1_${f}.fastq.gz.paired.sylsp -t 32 > SylphProfiles/${f}_ani_queries.tsv
-/usr/local/sylph profile  ~/Databases/BacterialDatabases/v0.3-c200-gtdb-r214.syldb Sketches/${f}.sylsp/trimmed_read1_${f}.fastq.gz.paired.sylsp -t 32 > SylphProfiles/${f}_profiled_bacteria.tsv
+sylph query  ~/Databases/BacterialDatabases/gtdb-r220-c200-dbv1.syldb Sketches/${f}.sylsp/trimmed_read1_${f}.fastq.gz.paired.sylsp -t 32 > SylphProfiles/${f}_ani_queries.tsv
+sylph profile  ~/Databases/BacterialDatabases/gtdb-r220-c200-dbv1.syldb Sketches/${f}.sylsp/trimmed_read1_${f}.fastq.gz.paired.sylsp -t 32 > SylphProfiles/${f}_profiled_bacteria.tsv
 
 
 # Viral profiling
-/usr/local/sylph profile ~/Databases/ViralDatabases/imgvr_c200_v0.3.0.syldb Sketches/${f}.sylsp/trimmed_read1_${f}.fastq.gz.paired.sylsp -t 24 > SylphProfiles/${f}_profiled_viruses.tsv 
+sylph profile ~/Databases/ViralDatabases/imgvr_c200_v0.3.0.syldb Sketches/${f}.sylsp/trimmed_read1_${f}.fastq.gz.paired.sylsp -t 24 > SylphProfiles/${f}_profiled_viruses.tsv 
 
 # the problem here is I don't know how to match up the contig name with virus
 # this is the portal for the viral database and one of them should have features that map to the contigs
 
 
 # Fungal profiling
-/usr/local/sylph profile ~/Databases/FungalDatabases/fungi-refseq-2023nov28-c200-v0.3.syldb Sketches/${f}.sylsp/trimmed_read1_${f}.fastq.gz.paired.sylsp -t 24 > SylphProfiles/${f}_profiled_fungi.tsv
+sylph profile ~/Databases/FungalDatabases/fungi-refseq-2023nov28-c200-v0.3.syldb Sketches/${f}.sylsp/trimmed_read1_${f}.fastq.gz.paired.sylsp -t 24 > SylphProfiles/${f}_profiled_fungi.tsv
 
 
 done
